97c76cc53a236f055d572ad8c0ba31f98de1c9c4
AkiQQ <akihuang0815@gmail.com>
2026-08-29 22:13:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aki-baseball-v19';
+const CACHE_NAME = 'aki-baseball-v20';
 const CORE_ASSETS = ['/', '/mobile/', '/manifest.json', '/favicon.ico', '/icon/icon-512x512.png'];
 
 self.addEventListener('install', (event) => {
